72b7329de726a893f47205a557c109e649b62d36
WangLinyu1 <39188942+WangLinyu1@users.noreply.github.com>
2019-08-05 17:08:59 -0700

    starter react

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -9,4 +9,17 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+fetch('http://www.mocky.io/v2/5cfc4b5c3000006f240a8c7d').then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+  })
+  .catch(function() {
+  	console.log("error") 
+	}
+  )
+;
+
+
+serviceWorker.register();
