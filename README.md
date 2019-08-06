6966237e4efb0966d297bc6d05405ffb69b4f2ef
WangLinyu1 <q491250161@163.com>
2019-08-05 17:32:31 -0700

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# traning
-html&amp;css
-test for html&css
---
+++ precache-manifest.2f5a0a4f6949fbbc0101041aea668786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062184a903e66aead57a167f698668ab",
+    "url": "/WangLinyu1/traning/tree/master/react/index.html"
+  },
+  {
+    "revision": "d6f9aa904c18d94f88c1",
+    "url": "/WangLinyu1/traning/tree/master/react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/WangLinyu1/traning/tree/master/react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d6f9aa904c18d94f88c1",
+    "url": "/WangLinyu1/traning/tree/master/react/static/js/main.92c405f1.chunk.js"
+  },
+  {
+    "revision": "da86e9ea10a42e2d3f4c",
+    "url": "/WangLinyu1/traning/tree/master/react/static/js/runtime~main.6e1caf58.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WangLinyu1/traning/tree/master/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
